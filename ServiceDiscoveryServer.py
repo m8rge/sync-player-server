b7c3ff267814a95a5697ba3ddf576cb80bbb82a2
m8rge <to.merge@gmail.com>
2012-02-04 00:03:50 +0600

    sds udp testing

--- ServiceDiscoveryServer.py
+++ ServiceDiscoveryServer.py
@@ -11,13 +11,11 @@ class ServiceDiscoveryServer:
             port+=1
 
         self.syncServerPort = syncServerPort
-        server = SocketServer.TCPServer((ip, port), self.getHandlerClass(syncServerPort, self.protocolVersion))
-#        server = SocketServer.UDPServer((ip, port), self.getHandlerClass(syncServerPort, self.protocolVersion))
+        server = SocketServer.UDPServer((ip, port), self.getHandlerClass(syncServerPort, self.protocolVersion))
         server.serve_forever()
 
     def isOpenUdpPort(self, ip, port):
-        s = socket.socket()
-#        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         try:
             s.connect((ip, int(port)))
             s.shutdown(socket.SHUT_RDWR)
@@ -30,17 +28,15 @@ class ServiceDiscoveryServer:
             def handle(self):
                 buffer = ''
                 while buffer == '' or (buffer.startswith('{') and '}' not in buffer) or len(buffer)>1024:
-                    buffer+= self.request.recv(1024)
-                    #data = self.request[0].strip()
+                    buffer+= self.request[0].strip()
 
                 try:
                     request = json.loads(buffer)
                     if request['command'] == 'hello':
                         ip, port = self.server.server_address
                         answer = dict({'ip': ip, 'port': syncServerPort, 'protocol': protocolVersion})
-                        self.request.send(json.dumps(answer))
-    #                    socket = self.request[1]
-    #                    socket.sendto(json.dumps(answer), self.client_address)
+                        socket = self.request[1]
+                        socket.sendto(json.dumps(answer), self.client_address)
                 except:
                     pass
         return Handler
