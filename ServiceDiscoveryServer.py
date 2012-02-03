61d1273ef27ea33bdb9912094fc4ac7ee2403b53
m8rge <to.merge@gmail.com>
2012-02-04 00:02:37 +0600

    sds tcp working

--- ServiceDiscoveryServer.py
+++ ServiceDiscoveryServer.py
@@ -5,15 +5,14 @@ import json
 class ServiceDiscoveryServer:
 
     protocolVersion = '1.0'
-    syncServerPort = 0
 
     def __init__(self, ip, port, syncServerPort):
         while self.isOpenUdpPort(ip, port):
             port+=1
 
         self.syncServerPort = syncServerPort
-        server = SocketServer.TCPServer((ip, port), self.handler)
-#        server = SocketServer.UDPServer((ip, port), self.handler)
+        server = SocketServer.TCPServer((ip, port), self.getHandlerClass(syncServerPort, self.protocolVersion))
+#        server = SocketServer.UDPServer((ip, port), self.getHandlerClass(syncServerPort, self.protocolVersion))
         server.serve_forever()
 
     def isOpenUdpPort(self, ip, port):
@@ -26,23 +25,25 @@ class ServiceDiscoveryServer:
         except:
             return False
 
-    class handler(SocketServer.BaseRequestHandler):
-        def handle(self):
-            buffer = ''
-            while buffer == '' or (buffer[0] == '{' and '}' not in buffer) or len(buffer)>1024:
-                buffer+= self.request.recv(1024)
-                #data = self.request[0].strip()
+    def getHandlerClass(self, syncServerPort, protocolVersion):
+        class Handler(SocketServer.BaseRequestHandler):
+            def handle(self):
+                buffer = ''
+                while buffer == '' or (buffer.startswith('{') and '}' not in buffer) or len(buffer)>1024:
+                    buffer+= self.request.recv(1024)
+                    #data = self.request[0].strip()
 
-            try:
-                request = json.loads(buffer)
-                if request['command'] == 'hello':
-                    ip, port = self.server.server_address
-                    answer = dict({'ip': ip, 'port': syncServerPort, 'protocol': protocolVersion})
-                    self.request.send(json.dumps(answer))
-#                    socket = self.request[1]
-#                    socket.sendto(json.dumps(answer), self.client_address)
-            except:
-                pass
+                try:
+                    request = json.loads(buffer)
+                    if request['command'] == 'hello':
+                        ip, port = self.server.server_address
+                        answer = dict({'ip': ip, 'port': syncServerPort, 'protocol': protocolVersion})
+                        self.request.send(json.dumps(answer))
+    #                    socket = self.request[1]
+    #                    socket.sendto(json.dumps(answer), self.client_address)
+                except:
+                    pass
+        return Handler
 
 if __name__ == "__main__":
     sdServer = ServiceDiscoveryServer('127.0.0.1', 45444, 80)
